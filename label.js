0ee899dbd80c7948c974f82bca1110fbe2c0256f
senarise <32718221+senarise@users.noreply.github.com>
2018-07-20 19:42:46 +0200

    Create label.js

---
+++ label.js
@@ -0,0 +1,3 @@
+<script type='text/javascript'>
+var numposts=1,numposts2=9,numposts3=7,showpostthumbnails=!0,showpostthumbnails2=!0,displaymore=!1,displaymore2=!1,showpostsummary=!0,numchars=150,thumb_width=310,thumb_height=160,thumb_width2=100,thumb_height2=55,no_thumb=&quot;http://3.bp.blogspot.com/-bP9BhHzcg3c/VulaYI1KgPI/AAAAAAAADlY/an6hlFHd5BQAUhNQbriNvTLripE4v5EQA/s320/mudwnp_thumb.png&quot;,no_thumb2=&quot;http://2.bp.blogspot.com/-9deKCtKRtvU/VulaYPTKW6I/AAAAAAAADlU/wb9d_4ysGPIC-ytki80VChZiMPrz7Af2w/s1600/mudwnpthumb_small.png&quot;;
+</script>
